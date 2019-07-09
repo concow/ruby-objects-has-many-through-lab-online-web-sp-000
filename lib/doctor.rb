944aa0d29e1ef1d2754a9abbdf1905d31e0df39c
Connor Cowan <ccowan003@comcast.net>
2019-07-09 05:23:21 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,12 +12,13 @@ class Doctor
     return @@all
   end
 
-  def new_appointment(patient, date)    #given a date and a patient, creates a new appointment
+  def new_appointment(patient, date)    #given a patient and a date, creates a new appointment
     Appointment.new(patient, self, date)    #Appointment knows it belongs to the Doctor (self)
   end
 
   def appointments                      #Class Doctor HAS MANY appointments
     Appointment.all.select {|appointment| appointment.doctor == self}
+    #instance method iterates through all Appointments selects those belonging to doctor instance     
   end
 
   def patients
