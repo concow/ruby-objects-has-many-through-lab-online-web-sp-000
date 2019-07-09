5e754477ac93ded993623794275659ea2211111a
Connor Cowan <ccowan003@comcast.net>
2019-07-09 05:05:51 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,6 +1,6 @@
 class Appointment
-  attr_accessor :date
-
+  attr_accessor :date, :doctor, :patient          #instance variables to create explicitly create appointments for doctor/patient
+                                                  #these connect relationships to deliver information on these related objects
   @@all = []
 
   def initialize(date)
